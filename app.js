3dcfad55cb2e3d8c9b7f75ee27d7c9c1954475af
pipatwina <pipatwina@msn.com>
2017-03-17 20:45:28 +0700

    Add Shema.sql

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var express = require('express')
 var app = express()
-app.listen(80)
+app.listen(2000)
+app.engine('html',require('ejs').renderFile)
+app.use(express.static('public'))
+app.use(showError)
+
+app.get('/',showHome)
+
+function showError(req,res){
+    res.render('error.html')
+}
+
+function showHome(req,res){
+    res.render('index.html')
+}
+
 app.get('/status',showStatus)
 
 function showStatus(req,res){
